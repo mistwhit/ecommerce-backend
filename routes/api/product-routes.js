2b466d996767a4061f3b2096b5b56cc213c01c93
Misty Whitenton <mistywhitenton@gmail.com>
2022-01-14 23:00:19 -0600

    Began writing product routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -7,12 +7,23 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  // TODO
+  try {
+    const productData = await Product.findAll({
+      include: [{ model: Category, model: Tag }],
+    });
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  // TODO
+  
 });
 
 // create new product
