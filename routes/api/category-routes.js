a3fa49a709200b7feeaf7b992e2e33e49a39a359
Misty Whitenton <mistywhitenton@gmail.com>
2022-01-14 22:52:05 -0600

    Finished up first attempt at category routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -42,11 +42,35 @@ router.post('/', (req, res) => {
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
-  
+  try {
+    const categoryData = await Category.update(
+      {
+        category_name: req.body.category_name,
+      },
+      {
+        where: {
+          id: req.params.id,
+        },
+      }
+    );
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  try {
+    const categoryData = await Category.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.status(200).json('Category deleted successfully!');
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
